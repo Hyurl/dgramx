db29ee6df0da0c93004446f8442c89689c48633a
hyurl <i@hyurl.com>
2018-05-03 21:37:33 +0800

    update dependencies, add CI configurations.

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const { createServer, createClient } = require("./");
+const assert = require("assert");
 
 let addr = "udp://127.0.0.1:12345";
 var server = createServer(addr);
@@ -6,9 +7,9 @@ var client = createClient(addr);
 
 server.on("listening", () => {
     let { address, port } = server.address();
-    console.log("Server listening at: %s:%d", address, port);
+    assert(typeof address == "string" && typeof port == "number" && port > 0);
 }).on("greet", (msg, rinfo) => {
-    console.log(msg); // => Hello, World!
+    assert.equal(msg, "Hello, World!");
 
     // reply
     server.to(rinfo.address, rinfo.port).emit("reply", "Message recieved.");
@@ -16,8 +17,10 @@ server.on("listening", () => {
 
 client.bind(0); // the client binds a random port and wait for reply.
 client.on("reply", (msg) => {
-    console.log(msg); // => Message recieved.
+    assert.equal(msg, "Message recieved.");
 
     client.close();
     server.close();
+
+    console.log("All tests passed!");
 }).emit("greet", "Hello, World!");
